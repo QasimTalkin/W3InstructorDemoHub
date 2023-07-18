8b9679a8c813f77476c7440b778d4952424c34ce
Abul Qasim <abulqasim110@gmail.com>
2023-07-18 10:35:57 -0400

    Jsss stuff

---
+++ JavaScript/utils/helper.js
@@ -0,0 +1,23 @@
+let student1 = "Kyle";
+
+let student2 = "Kyle 2";
+
+let student3 = "Steve";
+
+let student4 = "Kyle 4";
+
+let student5 = "Kyle 5";
+
+let student6 = "Kyle 6";
+
+//cons
+// cant iterate
+// too much work to add for every single student
+
+let students = [];
+
+students.pop("Steve");
+// million lines of code
+
+
+console.log(student3);
