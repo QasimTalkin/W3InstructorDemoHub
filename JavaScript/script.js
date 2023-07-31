a4408aeb41aa7d845430dd5b6d17df89dba9bf7a
Abul Qasim <abulqasim110@gmail.com>
2023-07-31 19:02:09 -0400

    js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,70 +1,115 @@
-// create an element -> h2, "Random Pic". id: subHeading ✔️
-// create an image element -> img, give it a url and other properties
-// addd these to the HTML document
+// var, let and const
+
+
+// spread and rest operator
+
+// let myArray = [1,2,3]
+
+// function sumOfThreeNums(a,b,c) {
+//   return a+b+c;
+// }
+
+// console.log(sumOfThreeNums(...myArray))
+
+
+// function iDontCareHowManyParams(...names) {
+//    for (let item of names){
+//      console.log(item)
+//    }
+// }
+
+// iDontCareHowManyParams("Qasim", "Kyle","Qasim", "Kyle","Qasim", "Kyle")
+
+
+// Destructuring
+
+// let person = {
+//   name:"Qasim",
+//   age:5,
+//   country:"Nicaragua",
+//   printInfo() {
+//   console.log(`Hello my name is ${this.name}, my age is ${this.age} and I live in ${this.country}, is Cool? ${this.isCool}`)
+//  },
+//  isCool:true
+// }
+
+// let {age, country, name} = person
+
+// let myNum = [1,2,3,4,5,6,7,9];
+
+// let [, , , , ...gender] = myNum;
+
+
+// console.log(gender);
+
+
+// Arrow Function
+
+// console.log(sum(5,9));
 
 
 
-let sub = document.createElement("h2");
-sub.innerText = "Random Pic ✔️";
-sub.id = "subHeading";
+// // declaration
+// function sum(a,b) {
+//   return a+b;
+// }
 
-let myImage = document.createElement("img") ; //<img src="">
-myImage.src = "https://picsum.photos/200/300";
-myImage.alt = "This is an image ok"
-myImage.id = "haoyu"
-
-
-// create a button element, give it the value "Stop Please don't' hurt me"
-// give it an id = "ourButton"
-
-// create an H2 element with id "counter" with innetText = 0
-
-// add event listener to the button that will update the h2 element with id counter
-
-let ourButton = document.createElement("button");
-ourButton.innerText = "Stop Please don't' hurt me";
-ourButton.id = "ourButton";
-
-let counter = 0;
-let ourH2 = document.createElement("h2");
-ourH2.id = "counter"
-ourH2.innerText = counter;
-
-document.body.append(ourButton);
-document.body.append(ourH2);
-
-let colors = ["red", "blue", "green", "pink", "yellow", "white", "black", "brown"]
-
-function updateCounter(event) {
-
-  alert(event.target.type);
-  alert(event.target.innerText);
-  counter++;
-  ourH2.innerText = counter;
-  document.body.style.background = colors[Math.floor((Math.random()*colors.length))]
-}
-ourButton.addEventListener("click", updateCounter)
-
-
-let inputOne = document.createElement("input");
-
-let inputTwo = document.createElement("input");
-
-let showName = document.createElement("button");
-showName.innerText = "Click me, Maybe Ill work";
-showName.addEventListener("click", displayName);
-
-
-let fullName = document.createElement("h2");
-fullName.innerText = "No Name";
-
-document.body.append(inputOne);
-document.body.append(inputTwo);
-
-document.body.append(showName);
-document.body.append(fullName);
-
-function displayName(){
-  fullName.innerText = Number(inputOne.value) + Number(inputTwo.value)
+let person = {
+  name:"Qasim",
+  age:5,
+  country:"Nicaragua",
+  info:"asd asdasdasd  asdnasdasjsdhnjn asdjnadjsdnadsjdn jnasd a ",
+  isCool:false
 }
 
+let person2 = {
+  name:"Kyle",
+  age:15,
+  country:"Nicaragua",
+  info:"asd asdasdasd  asdnasdasjsdhnjn asdjnadjsdnadsjdn jnasd a ",
+  isCool:true
+}
+
+let person3 = {
+  name:"Leo",
+  age:55,
+  country:"Nicaragua",
+  info:"some Info Classified ",
+  isCool:true
+}
+
+let person4 = {
+  name:"John Ault",
+  age:0,
+  country:"Nicaragua",
+  info:"Happy Birthday ",
+  isCool:true
+}
+
+let myPersonArray = [person, person2, person3, person4]
+
+function addCard(name, info, age) {
+  return `<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="https://picsum.photos/200/200" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${name}</h5>
+    <p class="card-text">${info}</p>
+    <p class="card-text">age is ${age}</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+  </div>`
+}
+
+myPersonArray.map((item)=>{
+if(item.isCool){
+  document.querySelector("#myCard").innerHTML += addCard(item.name, item.info, item.age)
+}
+})
+
+
+
+
+const sampleArr1 = [1, 2, 3, 4, 5];
+const sampleArr2 = sampleArr1.map(value=>value + 2);
+
+console.log(sampleArr2);
