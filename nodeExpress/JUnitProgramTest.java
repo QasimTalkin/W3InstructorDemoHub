36c1d2dd22dff5bd17f728f1a89320094b5bac4f
Abul Qasim <qasim@Abuls-MacBook-Pro.local>
2023-10-17 09:46:57 -0400

    FE BE link up

---
+++ nodeExpress/JUnitProgramTest.java
@@ -0,0 +1,14 @@
+import org.junit.*;
+
+
+public class JUnitProgramTest {
+
+@Test
+public void test() {
+
+  String result = "HelloWorld";
+
+  Assert.assertEquals("HelloWorld", result);
+  }
+
+}
--- nodeExpress/script.js
+++ nodeExpress/script.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const bcrypt = require("bcrypt");
-
 const nodeApp = express();
-nodeApp.use(express.json())
+const cors = require('cors');
+nodeApp.use(cors());
+
+nodeApp.use(express.json());
 const userRoutes = require("./routes/users")
 const loginRoutes = require("./routes/login")
 const postRoutes = require("./routes/post")
